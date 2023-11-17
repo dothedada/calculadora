b11bf08791950b527aa92ac5f858a1f068a7d4fa
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2023-11-16 22:41:59 -0500

    Habilita la creación de los valores a operar

--- main.js
+++ main.js
@@ -14,12 +14,48 @@ const memoria = document.querySelectorAll('.boton--memoria')
 const borrarMemoria = document.querySelectorAll('.boton--borrar')[1]
 const botonesOperacion = document.querySelectorAll('button[btn]')
 for(const numero of botonesOperacion) {
-    numero.addEventListener('click', ()=> {
-        console.log(operaciones[numero.textContent])
+    // constructor de un valor 
+    if(/\d|\./.test(numero.textContent)) numero.addEventListener('click', () => {
+        if(resultado.textContent.split(/\d/).length > 10) return
+        if(resultado.textContent === '0') {
+            resultado.textContent = numero.textContent
+            return
+        }
+        if(numero.textContent === '.' && resultado.textContent.includes('.')) {
+            resultado.textContent = resultado.textContent.split('.').join('')
+        }
+        resultado.textContent += numero.textContent
+    })
+    if(numero.textContent === '+/-') numero.addEventListener('click',() =>{
+        if(+resultado.textContent > 0) {
+            resultado.textContent = ['-', resultado.textContent].join('') 
+            return
+        }
+        if(+resultado.textContent < 0) resultado.textContent = resultado.textContent.slice(1)
+    })
+    // PENDIENTE
+    // paso del valor y asignacion de la operacio
+    if(/[^MC\d]/.test(numero.textContent) 
+        && numero.textContent.length < 3) numero.addEventListener('click', () => {
+        console.log(numero.textContent)
+        if(operador1.textContent === '0') {
+            operador1.textContent = resultado.textContent
+            operando.textContent = numero.textContent
+            resultado.textContent = '0'
+        }
     })
 }
+borrar.addEventListener('click', () => resultado.textContent = '0')
 
-const operaciones = {
+/* const operaciones = {
     'Xy': 'funciona',
-}
+    3RR0R
+}*/
+
+// calculadora
+// toma un valor
+// toma un operador
+// toma un segundo valor
+//
+
 
