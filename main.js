10891369d3ad427f1d3a0c1aeec5c3a3a93d3246
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2023-11-17 17:23:12 -0500

    incorpora el llamado de los botones y simplifica la función de escritura del número

--- main.js
+++ main.js
@@ -6,51 +6,45 @@ const panelRespuestas = document.getElementById('panelRespuestas')
 const operador1 = document.getElementById('operacion').firstElementChild
 const operador2 = document.getElementById('operacion').lastElementChild
 const operando = document.getElementById('operacion').children[1]
-const resultado = document.getElementById('resultado')
+const display = document.getElementById('resultado')
 
 // llamado de los elementos del teclado
-const borrar = document.querySelectorAll('.boton--borrar')[0]
-const memoria = document.querySelectorAll('.boton--memoria')
-const borrarMemoria = document.querySelectorAll('.boton--borrar')[1]
-const botonesOperacion = document.querySelectorAll('button[btn]')
-for(const numero of botonesOperacion) {
-    // constructor de un valor 
-    if(/\d|\./.test(numero.textContent)) numero.addEventListener('click', () => {
-        if(resultado.textContent.split(/\d/).length > 10) return
-        if(resultado.textContent === '0') {
-            resultado.textContent = numero.textContent
-            return
-        }
-        if(numero.textContent === '.' && resultado.textContent.includes('.')) {
-            resultado.textContent = resultado.textContent.split('.').join('')
-        }
-        resultado.textContent += numero.textContent
-    })
-    if(numero.textContent === '+/-') numero.addEventListener('click',() =>{
-        if(+resultado.textContent > 0) {
-            resultado.textContent = ['-', resultado.textContent].join('') 
-            return
-        }
-        if(+resultado.textContent < 0) resultado.textContent = resultado.textContent.slice(1)
-    })
-    // PENDIENTE
-    // paso del valor y asignacion de la operacio
-    if(/[^MC\d]/.test(numero.textContent) 
-        && numero.textContent.length < 3) numero.addEventListener('click', () => {
-        console.log(numero.textContent)
-        if(operador1.textContent === '0') {
-            operador1.textContent = resultado.textContent
-            operando.textContent = numero.textContent
-            resultado.textContent = '0'
-        }
-    })
-}
-borrar.addEventListener('click', () => resultado.textContent = '0')
+const botonesTeclado = document.getElementById('teclado')
 
-/* const operaciones = {
-    'Xy': 'funciona',
-    3RR0R
-}*/
+// funciones
+// Ingresar un dígito
+const ingresarDigito = digito => {
+    if(digito === '+/-' && +display.textContent){ 
+        display.textContent = -display.textContent
+    }
+    if(digito === '.') {
+        display.textContent = +display.textContent.split('.').join('')+'.'
+    }
+    if(display.textContent.split(/\d/).length > 10) return
+    if(/\d/.test(digito)) {
+        display.textContent = display.textContent === "0" ? digito : display.textContent + digito
+    }
+}
+// pasa el valor a la primera casilla
+
+
+// asignación de funciones al teclado
+botonesTeclado.addEventListener('click', evento => {
+    if(evento.target.tagName !== 'BUTTON') return
+    const boton = evento.target.textContent
+    // introducir un número
+    if(/\b\d|(\+\/-)|\./.test(boton)) ingresarDigito(boton)
+    
+    if(/[*Xy%=+\º/-]/.test(boton) && boton.length < 3) console.log(boton) // mejorar ese regex
+    
+    if(/[A-M]+\d|[A-M]/.test(boton)) console.log('memoria ' + boton)
+
+    // boton de borrar pantalla temporal
+
+    if(boton === 'C') display.textContent = 0
+})
+
+// Regex MEMORIA = /[A-M]+\d|[A-M]/
 
 // calculadora
 // toma un valor
